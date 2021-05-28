512d920dd5b53ea1ea1aaa2fecf5d6c3c950e66d
“MatheusSArantes” <matheus.sodre.arantes@gmail.com>
2021-05-28 10:53:16 -0300

    Desafio4

---
+++ mod.js
@@ -0,0 +1,14 @@
+function variavel(nome, idade, parâmetros){
+    console.log(`meu nome é ${nome}, tenho ${idade} anos de idade e gosto de musica ${parâmetros}`)
+    }
+    variavel("Matheus", 17, "indie")
+
+    function papel(filme, parâmetros){
+        console.log(`Meu filme favorito é ${filme} e eu tambem gosto de musica ${parâmetros}`)
+        }
+        papel("Viagem ao Centro da Terra", "indie")
+
+        function caneta( parâmetros){
+            console.log("por cada musica que eu escuto de uma banda, eu ouço " + (parâmetros * 3) + " vezes mais de outra banda" )
+            }
+            caneta(2)
